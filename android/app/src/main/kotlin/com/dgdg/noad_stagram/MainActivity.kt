81134ebd2d34af2eea0743f6d3822c058f2a29db
asher <asher@gtr.com.my>
2022-01-22 12:34:10 +0400

    init main

---
+++ android/app/src/main/kotlin/com/dgdg/noad_stagram/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dgdg.noad_stagram
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+          home : MyApp()
+      )
+  );
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold();
+
+  }
+}
